78358abb731dc920128327542e5f8ac66d4927b2
sardorbek <odiljonovsardorbek98@gmail.com>
2024-08-14 10:28:19 +0500

    Sardorbek, 1-commit, bottom navbar

--- lib/src/common/routing/app_route_name.dart
+++
@@ -1,9 +0,0 @@
-import "package:flutter/foundation.dart";
-
-@immutable
-final class AppRouteName {
-  const AppRouteName._();
-
-  static const String homePage = "/";
-  static const String profilePage = "/profile_page";
-}
--- lib/src/features/main/bloc/bottom_nav.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-// Bottom Navigation Bar Cubit
-class BottomNavCubit extends Cubit<int> {
-  BottomNavCubit() : super(0);
-
-  changeSelectedIndex(newIndex) => emit(newIndex);
-}
